5b79d3855cfffbf8154b65c6bd852d15ec142be3
Aryan <123654170+Aaru1007@users.noreply.github.com>
2024-09-09 17:08:51 +0530

    Add files via upload

---
+++ Guess The Word/server/coll.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+// Schema
+const roomdata = mongoose.Schema({
+  roomid: {
+    type: String,
+  }, 
+  
+  username: [
+    {
+      type: String,
+    },
+  ],
+});
+
+// Collection
+const rdata = mongoose.model('rdata', roomdata);
+
+export default rdata;
